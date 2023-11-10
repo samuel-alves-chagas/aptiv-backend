a2d5ab83ab15408ef2167f922e07fba81fe150d0
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2023-11-09 22:04:39 -0300

    Finalização da rota de login

--- controllers/apiControllerLogin.js
+++ controllers/apiControllerLogin.js
@@ -4,16 +4,13 @@ exports.Login = function(req, res, next) {
   const registroRecebido = req.body.registro
   const senhaRecebida = req.body.senha
 
-  Colaborador.find({registro: registroRecebido}).then(function(colaborador){
-    console.log(colaborador, senhaRecebida)
-
-    if(colaborador.senha == senhaRecebida) {
+  Colaborador.find({registro: registroRecebido, senha: senhaRecebida}).then(function(colaborador){
+    if(colaborador[0]) {
       return res.send(200);
     }
-    return res.send(400)
+    return res.json({error:"Registro ou senha inválidos"}).send(404)
   }).catch((err) => {
     console.log(err)
+    next()
   });
-
-
 }
