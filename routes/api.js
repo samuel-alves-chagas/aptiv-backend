b7219cf705683458c9917aeb900cff217ea6045a
Daiane Dionisio de Lima <daianedionisio2000@gmail.com>
2023-11-27 22:34:41 -0300

    Update Rota para Buscar Holerite

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,7 @@ router.get('/documento/:id',apiControllerDocumento.getDocumento);
 router.post('/documento',apiControllerDocumento.createDocumento);
 router.put('/documento/:id',apiControllerDocumento.updateDocumento);
 router.delete('/documento/:id',apiControllerDocumento.deleteDocumento);
-router.get('/holerite', apiControllerDocumento.getDocumentoByDataEColaborador);
+router.post('/holerite', apiControllerDocumento.getDocumentoByDataEColaborador);
 
 router.get('/unidade/:id',apiControllerUnidade.getUnidade);
 router.post('/unidade',apiControllerUnidade.createUnidade);
